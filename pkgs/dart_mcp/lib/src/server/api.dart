52c8a98d45b5197b567323704a5ae7542878f5a1
Jake Macdonald <jakemac@google.com>
2025-03-20 12:29:51 -0700

    add initial server API and IO impl

---
+++ pkgs/dart_mcp/lib/src/server/api.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:stream_channel/stream_channel.dart';
+import 'package:json_rpc_2/json_rpc_2.dart';
+
+abstract class MCPServer {
+  late final Peer _peer;
+
+  MCPServer.fromStreamChannel(StreamChannel<String> channel)
+    : _peer = Peer(channel);
+}
