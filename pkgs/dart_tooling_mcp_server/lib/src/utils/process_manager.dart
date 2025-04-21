d1acecef637b473f08ba65ab4e83b567503a1f9c
Kenzie Davisson <kenzieschmoll@google.com>
2025-04-21 16:26:09 -0700

    dart doc

--- pkgs/dart_tooling_mcp_server/lib/src/utils/process_manager.dart
+++ pkgs/dart_tooling_mcp_server/lib/src/utils/process_manager.dart
@@ -6,14 +6,15 @@ import 'dart:io';
 
 import 'package:process/process.dart';
 
-/// A mixin that provides a single getter of type [LocalProcessManager].
+/// An interface class that provides a single getter of type
+/// [LocalProcessManager].
 ///
-/// The `DartToolingMCPServer` class mixes this in so that [Process] methods
-/// can be easily mocked during testing.
+/// The `DartToolingMCPServer` class implements this class so that [Process]
+/// methods can be easily mocked during testing.
 ///
-/// MCP support mixins like `DartCliSupport` that spawn processes should use
-/// [processManager] from this mixin instead of making direct calls to dart:io's
-/// [Process] class.
+/// MCP support mixins like `DartCliSupport` that spawn processes should also
+/// implement this class and use [processManager] instead of making direct calls
+/// to dart:io's [Process] class.
 abstract interface class ProcessManagerSupport {
   LocalProcessManager get processManager;
 }
