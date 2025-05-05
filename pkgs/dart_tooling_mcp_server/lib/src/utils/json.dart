29888c829e9bb9cfb5c8c5c509c03fd4768bc19b
Sigurd Meldgaard <sigurdm@google.com>
2025-05-05 15:55:41 +0200

    Test and fix json dig

--- pkgs/dart_tooling_mcp_server/lib/src/utils/json.dart
+++ pkgs/dart_tooling_mcp_server/lib/src/utils/json.dart
@@ -23,40 +23,86 @@
 /// Returns the result as a [T].
 T dig<T>(dynamic json, List<dynamic> path) {
   var i = 0;
-  String currentPath() => path.take(i).map((i) => '[$i]').join('');
+  String currentElementType() => switch (json) {
+    Null _ => 'null',
+    num _ => 'a number',
+    String _ => 'a string',
+    List _ => 'a list',
+    Map _ => 'a map',
+    _ => throw ArgumentError('Bad json'),
+  };
+  String currentPath() =>
+      i == 0 ? 'root' : path.sublist(0, i).map((i) => '[$i]').join('');
   for (; i < path.length; i++) {
     outer:
     switch (path[i]) {
       case final String key:
         if (json is! Map) {
-          throw FormatException('Expected a map at ${currentPath()}');
+          throw FormatException(
+            'Expected a map at ${currentPath()}. '
+            'Found ${currentElementType()}.',
+          );
         }
         json = json[key];
       case final int key:
         if (json is! List) {
-          throw FormatException('Expected a map at ${currentPath()}');
+          throw FormatException(
+            'Expected a list at ${currentPath()}. '
+            'Found ${currentElementType()}.',
+          );
+        }
+        if (key >= json.length) {
+          throw FormatException(
+            'Expected at least ${key + 1} element(s) at ${currentPath()}. '
+            'Found only ${json.length} element(s)',
+          );
         }
         json = json[key];
       case (final String key, final String value):
         if (json is! List) {
-          throw FormatException('Expected a map at ${currentPath()}');
+          throw FormatException(
+            'Expected a list at ${currentPath()}. '
+            'Found ${currentElementType()}.',
+          );
         }
         final t = json;
         for (var j = 0; j < t.length; j++) {
           final element = t[j];
           if (element is! Map) {
-            throw FormatException('Expected a map at ${currentPath()}[$j]');
+            json = element;
+            throw FormatException(
+              'Expected a map at ${currentPath()}[$j]. '
+              'Found ${currentElementType()}.',
+            );
           }
           if (element[key] == value) {
             json = element;
             break outer;
           }
         }
+        throw FormatException(
+          'No element with $key=$value at ${currentPath()}',
+        );
       case final key:
         throw ArgumentError('Bad key $key in', 'path');
     }
   }
 
-  if (json is! T) throw FormatException('Unexpected value at $currentPath()');
+  if (json is! T) {
+    final targetTypeName = switch (T) {
+      const (int) => 'an int',
+      const (double) => 'a number',
+      const (num) => 'a number',
+      const (String) => 'a string',
+      const (List) => 'a list',
+      const (Map) => 'a map',
+      const (Null) => 'null',
+      _ => throw ArgumentError('$T is not a json type'),
+    };
+    throw FormatException(
+      'Expected $targetTypeName at ${currentPath()}. '
+      'Found ${currentElementType()}.',
+    );
+  }
   return json;
 }
