a779f1cf57f00aec65a2031e3e61cb27dd458df6
Aaron Clarke <aaclarke@google.com>
2025-07-09 12:42:50 -0700

    removed unused import

--- pkgs/dart_mcp/test/api/initialization_test.dart
+++ pkgs/dart_mcp/test/api/initialization_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:js_interop';
-
 import 'package:dart_mcp/server.dart';
 import 'package:test/test.dart';
 
