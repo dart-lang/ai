96b4fb70ceef8471a5d9ee908d46b3db36bd7d44
Greg Spencer <gspencer@google.com>
2025-10-06 13:08:19 -0700

    Update copyright notices

--- pkgs/dart_mcp_server/lib/src/mixins/flutter_launcher.dart
+++ pkgs/dart_mcp_server/lib/src/mixins/flutter_launcher.dart
@@ -1,6 +1,6 @@
-// Copyright 2025 The Flutter Authors.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 /// A mixin that provides tools for launching and managing Flutter applications.
 library;
