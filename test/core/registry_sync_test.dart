63d66bb7636b485d0d4b2607a5f5b8ff3810ec90
Viktor Lidholt <viktorlidholt@gmail.com>
2026-03-07 16:58:31 +0100

    chore: Dart format.

--- test/core/registry_sync_test.dart
+++ test/core/registry_sync_test.dart
@@ -27,16 +27,16 @@ void main() {
           equals(0),
         );
         // Required for git commit in CI (no global user.name/user.email).
-        await Process.run(
-          'git',
-          ['config', 'user.email', 'test@test'],
-          workingDirectory: localPath,
-        );
-        await Process.run(
-          'git',
-          ['config', 'user.name', 'Test'],
-          workingDirectory: localPath,
-        );
+        await Process.run('git', [
+          'config',
+          'user.email',
+          'test@test',
+        ], workingDirectory: localPath);
+        await Process.run('git', [
+          'config',
+          'user.name',
+          'Test',
+        ], workingDirectory: localPath);
         await Process.run('git', ['add', '.'], workingDirectory: localPath);
         expect(
           (await Process.run('git', [
