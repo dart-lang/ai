0b8e661347f6168ba235256e4621b281b64eaceb
Jacob MacDonald <jakemac@google.com>
2026-07-17 08:08:26 -0700

    publish package:skills 1.0.0-beta.1 (#527)
    This release will allow us to get a few dogfooders using the package like `dart run skills@1.0.0-beta.1` or `dart run skills@^1.0.0.beta`
    
    - Also cleans up the README a bit
    - And includes the agent aliases in the agent selection dialog

--- pkgs/skills/test/commands/get_command_select_skills_test.dart
+++ pkgs/skills/test/commands/get_command_select_skills_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:io';
 
 import 'package:logging/logging.dart';
@@ -52,7 +56,6 @@ void main() {
 
         final projectRootDir = d.dir('project', [
           pubspec('project', dependencies: [.new('dep1'), .new('dep2')]),
-          d.dir('.cursor', [d.dir('skills')]),
         ]);
         await projectRootDir.create();
 
@@ -204,7 +207,6 @@ void main() {
 
         final projectRootDir = d.dir('project', [
           pubspec('project', dependencies: [.new('dep1'), .new('dep2')]),
-          d.dir('.cursor', [d.dir('skills')]),
         ]);
         await projectRootDir.create();
 
